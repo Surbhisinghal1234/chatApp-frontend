9177ebbbe6e2badb4ca2e69607ef4397a9e3d780
Surbhi Singhal <surbhi98singhal@gmail.com>
2024-08-26 23:10:42 +0530

    working on login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,6 +4,7 @@ import useLogin from "../../hooks/useLogin";
 import ThemeToggle from "../../components/theme/ThemeToggle";
 import { useTheme } from "../../hooks/useTheme";
 import "./login.css";
+import img1 from "../../assets/robot.png"
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -17,49 +18,62 @@ const Login = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen login-bg">
-      <div className="flex flex-col gap-[2rem] items-center md:p-12 p-6 rounded-lg max:w-[28rem] login-content">
+    <div className="flex flex-col justify-center items-center h-screen login-bg">
+
+     
         <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
+
+      <div className="flex login-content rounded-lg items-center justify-center p-10 gap-10">
+      <div className="login-left-bg h-[20rem] w-[15rem] hidden md:flex">
+          <img src={img1} alt="" className="h-[100%] w-[100%] object-cover" />
+      </div>
+      <div className=" flex flex-col gap-10">
         <h1 className={`text-xl sm:text-2xl md:text-3xl font-semibold text-center ${theme === "light" ? "text-slate-700" : "text-white"}`}>
           Login to
           <span className={`text-black inline-block ml-4  ${theme === "light" ? "text-slate-700" : "text-white"}`}>Chat Application</span>
         </h1>
         <form onSubmit={handleSubmit} className="flex flex-col gap-6">
           <div className="flex flex-col gap-2">
-            <label htmlFor="email" className="text-lg font-medium">Email</label>
+            {/* <label htmlFor="email" className="text-lg font-medium">Email</label> */}
             <input
               type="email"
               id="email"
-              placeholder="Enter Email"
-              className="outline-none rounded-md px-3 py-2 w-full sm:w-auto shadow-sm shadow-[#f6d4f579] input-field"
+              placeholder="Email"
+              className="outline-none border-white border-2 rounded-3xl px-5 py-2 w-full sm:w-auto shadow-sm shadow-[#f6d4f579] input-field"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
           <div className="flex flex-col gap-2">
-            <label htmlFor="password" className="text-lg font-medium">Password</label>
+            {/* <label htmlFor="password" className="text-lg font-medium">Password</label> */}
             <input
               type="password"
               id="password"
-              placeholder="Enter Password"
-              className="outline-none rounded-md px-3 py-2 w-full sm:w-auto shadow-sm shadow-[#f6d4f579] input-field"
+              placeholder="Password"
+              className="outline-none rounded-3xl border-white border-2 px-5 py-2 w-full sm:w-auto shadow-sm shadow-[#f6d4f579] input-field"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
-          <Link className="text-lg font-medium hover:underline hover:text-black" to="/signup">
-            {"Don't "} have an account?
-          </Link>
+          
           <button
             type="submit"
-            className={`btn rounded-full px-8 py-2 font-medium text-white border-none ${loading ? "bg-gray-400" : "bg-blue-500"}`}
+            className={`btn btn-same rounded-full font-bold  px-8  text-white border-none ${loading ? "bg-gray-400" : "bg-blue-500"}`}
             disabled={loading}
           >
-            {loading ? <span className="loading loading-spinner"></span> : "Login"}
+            {loading ? <span className="loading  loading-spinner"></span> : "Login"}
           </button>
+          <Link className={`text-lg font-medium hover:underline  ${theme==="light"? "hover:text-black" : "hover:text-gray-500"}`}   to="/signup">
+            {"Don't "} have an account?
+          </Link>
+          <Link className="btn btn-same  rounded-full text-center px-8 py-2 font-medium border-none" to="/signup">
+            Sign Up
+          </Link>
+
         </form>
+        </div>
       </div>
     </div>
   );
